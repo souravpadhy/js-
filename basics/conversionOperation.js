467bf477c60f0aaf3fcdd98b6dce948078950f50
souravpadhy <51479460+souravpadhy@users.noreply.github.com>
2024-06-03 05:39:27 +0000

    changes

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,1 +1,28 @@
-let score = 33
+let score = "sourav"
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"45" => 45
+//"33abc" => NaN
+//true/ false => 1 and 0
+
+
+let isLoggedIn = "sourav"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true and o - false
+//" " => false
+//"sourav"=> true
+
+let num = 78
+let stringNUmber = String(num)
+console.log(stringNUmber);
+console.log(typeof stringNUmber);
