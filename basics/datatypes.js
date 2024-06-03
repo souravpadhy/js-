eb3201c8f24844dc1bb91fff4efd3df3bfde5a27
souravpadhy <51479460+souravpadhy@users.noreply.github.com>
2024-06-03 05:26:52 +0000

    commit changes.

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // use all code as newer version of js
+
+//alert(3+3) //this is not browser, we r using node js
+
+let name = "sourav"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number 2 to power 53
+//bigint 
+//string => " "
+//boolean => true or false
+//null => standalone value
+//undefined 
+//symbol => unique
+
+//object 
+
+console.log(typeof undefined); //undefined type
+console.log(typeof null);//object type
