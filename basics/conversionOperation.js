337b762a694690348999a57cec7ea0cc08dc32d9
souravpadhy <51479460+souravpadhy@users.noreply.github.com>
2024-06-03 05:48:52 +0000

    commit

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -26,3 +26,23 @@ let num = 78
 let stringNUmber = String(num)
 console.log(stringNUmber);
 console.log(typeof stringNUmber);
+
+////*****************   OPEARTIONS *******************/
+
+let value = 3
+
+let negValue = - value
+console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " sourav"
+
+let str3= str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
