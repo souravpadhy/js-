eb40856c22f3c18de26ae710db845d2f1e3ae853
souravpadhy <51479460+souravpadhy@users.noreply.github.com>
2024-06-03 05:55:42 +0000

    commit

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -46,3 +46,15 @@ console.log("1" + 2);
 console.log(1 + "2");
 console.log("1" + 2 + 2);
 console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+
+let num1, num2 ,num3
+
+num1=num2=num3 =2+2
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter)
