c096d3e8d7260cadccbf27111a387d0e77b50910
souravpadhy <51479460+souravpadhy@users.noreply.github.com>
2024-06-05 04:14:38 +0000

    changes

--- basics/comparison.js
+++ basics/comparison.js
@@ -7,3 +7,9 @@ console.log("02">1);
 console.log(null>0);
 console.log(null==0); //null is assigned as 0.
 console.log(null>=0); // hence it is true
+
+
+// === strict check ...it strictly check value and datatype
+
+
+console.log("2"===2);
---
+++ basics/datatypes_summary.js
@@ -0,0 +1,10 @@
+// primitive
+ //they r call by value
+
+//js is dynamic, we donot have to assign number,boolean,float etc
+
+// 7 types : string , number , value , Boolean , null , undefined ,Symbol , BigInt
+
+//Refrence / Non-Primtive type: Arrays, Objects , Functions
+
+
